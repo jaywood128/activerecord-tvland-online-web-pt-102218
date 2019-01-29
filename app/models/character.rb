abf2a0b805f3208949fdf733b6c244bc0a4b9b58
Johnathon Wood <jaywood128@gmail.com>
2019-01-29 18:11:48 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,6 +4,6 @@ class Character < ActiveRecord::Base
   def say_that_thing_you_say
     Character.all.collect do |character|
       "#{character.name} always says: #{character.catchphrase}"
-    end
+    end[0]
   end
 end
