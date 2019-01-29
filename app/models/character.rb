593ecbb1e08336af4291518dd49433d03673ca9c
Johnathon Wood <jaywood128@gmail.com>
2019-01-29 18:10:06 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,10 @@
 class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
+  def say_that_thing_you_say
+    Character.all.collect do |character|
+      binding.pry
+      "#{character.name} always says: #{character.catchphrase}"
+    end
+  end
 end
